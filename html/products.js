782a089bf76d9798b72fde50aa0f0e279e9f53bf
damiansetunge <s3828462@student.rmit.edu.au>
2022-10-13 10:33:33 +1100

    Cart Functionality Implemented

---
+++ html/products.js
@@ -0,0 +1,91 @@
+const products = [
+  {
+    id: 0,
+    name: "T-shirts",
+    price: 29.99,
+    instock: 100,
+    description:
+      "Cotton T-shirt : All Sexes",
+    imgSrc: "./img/t1.png",
+  },
+  {
+    id: 1,
+    name: "Formal Shirts",
+    price: 24.99,
+    instock: 43,
+    description:
+      "Formal/Imformal : Mens",
+    imgSrc: "./img/t2.png",
+  },
+  {
+    id: 2,
+    name: "Formal Blouses",
+    price: 19.99,
+    instock: 10,
+    description:
+      "Formal/Imformal: Womens",
+    imgSrc: "./img/t3.png",
+  },
+  {
+    id: 3,
+    name: "Jeans",
+    price: 25.99,
+    instock: 5,
+    description:
+      "Jeans : All Sexes",
+    imgSrc: "./img/t4.png",
+  },
+  {
+    id: 4,
+    name: "Pants/Trouses ",
+    price: 29.99,
+    instock: 4,
+    description:
+      "Formal/Infromal : All Sexes ",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 5,
+    name: "Jackets",
+    price: 39.99,
+    instock: 40,
+    description:
+      "Hoodies/Jackets : All Sexes",
+    imgSrc: "./img/t6.png",
+  },
+  ,
+  {
+    id: 6,
+    name: "Mens Underwear",
+    price: 39.99,
+    instock: 40,
+    description:
+      "Brief/Boxers : Mens",
+    imgSrc: "./img/t7.png",
+  },
+
+  ,
+  {
+    id: 7,
+    name: "Ladies Underwear Set",
+    price: 39.99,
+    instock: 40,
+    description:
+      "Bra and Panty Set : Womens",
+    imgSrc: "./img/t8.png",
+  },
+  ,
+  {
+    id: 8,
+    name: "Socks Set",
+    price: 39.99,
+    instock: 40,
+    description:
+      "All Sexes",
+    imgSrc: "./img/t9.png",
+  },
+
+  
+  
+  
+];
